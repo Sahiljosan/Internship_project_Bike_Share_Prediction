33987b7b4eaeebfbfff1a35869e1dab47c54f05a
Sahil Josan <sahiljosan50@gmail.com>
2023-06-10 11:37:17 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,29 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import base64
+
+col1, col2 = st.columns(2)
+with col1:
+       def add_bg_from_local(image_file):
+             
+              with open(image_file, "rb") as image_file:
+                     encoded_string = base64.b64encode(image_file.read())
+              st.markdown(
+              f"""
+              <style>
+              .stApp {{
+                     background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+                     background-size: cover
+              }}
+              </style>
+              """,
+              unsafe_allow_html=True
+              )
+
+
+       add_bg_from_local('BG Image.jpg')  
+
 
 # From here code for our project starts
 
@@ -10,39 +33,40 @@ encoder = pickle.load(open('target_encoder.pkl','rb'))
 transformer = pickle.load(open('transformer.pkl','rb'))
 
 
-st.title("Bike Share Prediction")
+with col2:
+      
+       st.title("Bike Share Prediction")
 
+       season = st.selectbox("Please Select the season",("spring","summer","fall","winter"))
 
-season = st.selectbox("Please Select the season",("spring","summer","fall","winter"))
+       yr = st.selectbox("Select the year",(2011, 2012))
 
-yr = st.selectbox("Select the year",(2011, 2012))
+       mnth = st.selectbox("Select the month",('Jan','feb','mar','april', 'may','june','july','aug','sep','oct', 'nov', 'dec'))
 
-mnth = st.selectbox("Select the month",('Jan','feb','mar','april', 'may','june','july','aug','sep','oct', 'nov', 'dec'))
+       holiday = st.selectbox("Is there a holiday",('yes','no'))
 
-holiday = st.selectbox("Is there a holiday",('yes','no'))
+       weekday = st.selectbox("Select Weekday", ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday',
+              'friday','saturday'))
 
-weekday = st.selectbox("Select Weekday", ('saturday', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday',
-       'friday'))
+       workingday = st.selectbox("Is Working day or not",('yes','no'))
 
-workingday = st.selectbox("Is Working day or not",('yes','no'))
+       weathersit = st.selectbox("Select weathersit",(2,1,3))
 
-weathersit = st.selectbox("Select weathersit",(2,1,3))
+       temp = st.text_input("Enter temperature between (0 - 0.5)",0.363)
+       temp = float(temp)
 
-temp = st.text_input("Enter temperature between 0 - 0.5",0.363)
-temp = float(temp)
+       atemp = st.text_input("Enter atemperature between (0 - 0.5)",0.189)
+       atemp = float(atemp)
 
-atemp = st.text_input("Enter atemperature between 0 - 0.5",0.189)
-atemp = float(atemp)
+       hum = st.text_input("Enter humadity between (0 - 0.8)",0.805)
+       hum = float(hum)
 
-hum = st.text_input("Enter humadity between 0 - 0.8",0.805)
-hum = float(hum)
+       windspeed = st.text_input("Enter the windspeed (0 - 0.5)",0.304)
+       windspeed = float(windspeed)
 
-windspeed = st.text_input("Enter the windspeed",0.304)
-windspeed = float(windspeed)
+       casual = st.text_input("Enter casual number (0 - 500)",222)
 
-casual = st.text_input("Enter casual number (0 - 500)",222)
-
-registered = st.text_input("Enter registered number (0-1600)",891)
+       registered = st.text_input("Enter registered number (0-1600)",891)
 
 
 # We have to store all in dictionary
@@ -66,11 +90,15 @@ l['registered'] = registered
 df = pd.DataFrame(l,index=[0])
 
 # we do encoding of all features
-df['season'] = encoder.transform(df['season'])
-df['mnth'] = encoder.transform(df['mnth'])
+# df['season'] = encoder.transform(df['season'])
+# df['mnth'] = encoder.transform(df['mnth'])
 df['holiday'] = df['holiday'].map({'yes':1,'no':0})
-df['weekday'] = encoder.transform(df['weekday'])
+# df['weekday'] = encoder.transform(df['weekday'])
 df['workingday'] = df['workingday'].map({'yes':1,'no':0})
+df['season'] = df['season'].map({'spring':0, 'summer':1,'fall':2,'winter':3})
+df['mnth'] = df['mnth'].map({'Jan':0,'feb':1,'mar':2,'april':3, 'may':4,'june':5,'july':6,'aug':7,'sep':8,'oct':9, 'nov':10, 'dec':11})
+df['weekday'] = df['weekday'].map({'sunday':0, 'monday':1, 'tuesday':2, 'wednesday':3, 'thursday':4,'friday':5,'saturday':6})
+
 
 
 df = transformer.transform(df)
@@ -78,5 +106,6 @@ df = transformer.transform(df)
 y_pred = model.predict(df)
 
 # Add Submit Button
-if st.button("Show Counts"):
-    st.header(f"{y_pred}")
+with col2:
+       if st.button("Show Counts"):
+              st.header(f"{round(y_pred[0])} bikes will be shared")
