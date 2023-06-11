ebce3e2dcbecaecd5e762d67ab4b9dd6602b76d0
Sahil Josan <sahiljosan50@gmail.com>
2023-06-11 09:50:02 +0530

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def add_bg_from_local(image_file):
        
        with open(image_file, "rb") as image_file:
               encoded_string = base64.b64encode(image_file.read())
-       st.sidebar.markdown(
+       st.markdown(
        f"""
        <style>
        .stApp {{
